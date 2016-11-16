e6d77b63f26be974b6ab8cfd459b3e67257d3443
Nmuta Jones <nmuta1@gmail.com>
2016-11-16 15:56:18 -0700

    typo

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -3,12 +3,12 @@ class Partner < ActiveRecord::Base
   has_many :events
 
   def self.get_person_by_id(who_with_id)
-    person = Person.where(id: who_with_id)
+    person = self.where(id: who_with_id)
     found = person ? person.first.name : null
   end
 
   def self.get_person_by_name(who_with)
-    person = Person.where(name: who_with)
+    person = self.where(name: who_with)
     found = person ? person.first.id : null
   end
 end
