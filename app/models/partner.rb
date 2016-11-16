080b51d395cfa806ab7b49bb221e64b7cd68f105
Nmuta Jones <nmuta1@gmail.com>
2016-11-16 15:53:10 -0700

    make partner methods static (class based)

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,12 +2,12 @@ class Partner < ActiveRecord::Base
 
   has_many :events
 
-  def get_person_by_id(who_with_id)
+  def self.get_person_by_id(who_with_id)
     person = Person.where(id: who_with_id)
     found = person ? person.first.name : null
   end
 
-  def get_person_by_name(who_with)
+  def self.get_person_by_name(who_with)
     person = Person.where(name: who_with)
     found = person ? person.first.id : null
   end
