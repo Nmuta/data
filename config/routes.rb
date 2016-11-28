bdabc9f3f8528e36f7e25bf7074a11aff8fa355e
Nmuta Jones <nmuta1@gmail.com>
2016-11-27 19:48:55 -0700

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'tips#index'
+  root 'home#index'
 
 
   get 'get_tip' => 'tips#get_tip'
